4ccaa76f2a7cc530b35aed5acc550ae268ce761f
diejofre <diego-jofre@live.com.ar>
2023-02-14 12:02:04 -0300

    react icons incorporado

--- 4/ejemploComponentes/src/components/Navbar/Navbar.jsx
+++ 4/ejemploComponentes/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import Button from "../Button/Button";
 import styles from "./navbar.module.css";
+import { BsCart4 } from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <nav className={styles.container}>
+      {/* <CartWidget /> */}
       <p>Home</p>
       <p>About</p>
       <p>Projects</p>
-      <Button txtBtn="Navbar" />
+      <BsCart4 />
     </nav>
   );
 };
